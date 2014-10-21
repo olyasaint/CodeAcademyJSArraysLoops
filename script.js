14e77bc689def7f4227833f587f4afc14203caba
olyasaint <olya@ostryk.com>
2014-10-21 16:39:38 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//Create an array called names filled with 5 names.
+Write a for loop that prints "I know someone called " followed by names[i]. 
+
+var names = ["Nadia", "Francis", "Vera", "Jeff", "Gregory"];
+for (var i =  0; i < names.length; i++) {
+    console.log ("I know someone called " + names[i]);
+}
